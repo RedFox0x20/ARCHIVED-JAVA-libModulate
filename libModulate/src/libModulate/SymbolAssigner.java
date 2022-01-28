1c0812b2321a9709c308dce8397a9a4edc3ce908
Red (RedFox0x20) <redfox0x20@gmail.com>
2022-01-28 22:03:22 +0000

    Default assignments
    Added default assignments for bits and symbol conversions

--- libModulate/src/libModulate/SymbolAssigner.java
+++ libModulate/src/libModulate/SymbolAssigner.java
@@ -27,6 +27,7 @@ public class SymbolAssigner {
 		while (BitIndex < bits.length) {
 			byte[] ComparisonBits = new byte[getBitsPerSymbol()];
 			for (int i = 0; i < getBitsPerSymbol(); i++) {
+				if (BitIndex >= bits.length) { break; }
 				ComparisonBits[i] = bits[BitIndex++];
 			}
 			Boolean Found = false;
@@ -96,4 +97,38 @@ public class SymbolAssigner {
 			new Assignment_ManyToOne(new byte[] { 1, 1, 0, 1 }, (byte) 13),
 			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 0 }, (byte) 14),
 			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 1 }, (byte) 15) };
+
+	public static final Assignment_ManyToOne[] DEFAULT_ASSIGNMENT_MFSK32_SYMBOLS = new Assignment_ManyToOne[] {
+			new Assignment_ManyToOne(new byte[] { 0, 0, 0, 0, 0 }, (byte) 0),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 0, 0, 1 }, (byte) 1),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 0, 1, 0 }, (byte) 2),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 0, 1, 1 }, (byte) 3),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 1, 0, 0 }, (byte) 4),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 1, 0, 1 }, (byte) 5),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 1, 1, 0 }, (byte) 6),
+			new Assignment_ManyToOne(new byte[] { 0, 0, 1, 1, 1 }, (byte) 7),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 0, 0, 0 }, (byte) 8),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 0, 0, 1 }, (byte) 9),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 0, 1, 0 }, (byte) 10),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 0, 1, 1 }, (byte) 11),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 1, 0, 0 }, (byte) 12),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 1, 0, 1 }, (byte) 13),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 1, 1, 0 }, (byte) 14),
+			new Assignment_ManyToOne(new byte[] { 0, 1, 1, 1, 1 }, (byte) 15),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 0, 0, 0 }, (byte) 16),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 0, 0, 1 }, (byte) 17),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 0, 1, 0 }, (byte) 18),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 0, 1, 1 }, (byte) 19),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 1, 0, 0 }, (byte) 20),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 1, 0, 1 }, (byte) 21),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 1, 1, 0 }, (byte) 22),
+			new Assignment_ManyToOne(new byte[] { 1, 0, 1, 1, 1 }, (byte) 23),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 0, 0, 0 }, (byte) 24),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 0, 0, 1 }, (byte) 25),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 0, 1, 0 }, (byte) 26),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 0, 1, 1 }, (byte) 27),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 0, 0 }, (byte) 28),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 0, 1 }, (byte) 29),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 1, 0 }, (byte) 30),
+			new Assignment_ManyToOne(new byte[] { 1, 1, 1, 1, 1 }, (byte) 31) };
 }
