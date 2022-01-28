414a4be7f6838e088e2c03f96bfde9767c96664d
Red (RedFox0x20) <redfox0x20@gmail.com>
2022-01-28 21:20:48 +0000

    Bytes to hex
    Rather than ASCII, hex is more useful so the method has been adapted, a new ASCII method should be implemented in the future

--- libModulate/src/libModulate/utils/DataModifiers.java
+++ libModulate/src/libModulate/utils/DataModifiers.java
@@ -4,13 +4,15 @@ public class DataModifiers {
 	// ByteArrayToASCII
 	// Iterates over byte[] Data and converts all values in the range of 0-9 to
 	// their ASCII equivalent (n + '0')
-	public static byte[] ByteArrayToASCII(byte[] Data) {
+	public static byte[] ByteArrayToHex(byte[] Data) {
 		byte[] Results = new byte[Data.length];
 
 		for (int i = 0; i < Results.length; i++) {
 			byte Original = Data[i];
 			if (Original >= 0 && Original <= 9) {
 				Results[i] = (byte) (Original + (byte) '0');
+			} else if (Original >= 10 && Original <= 15) {
+				Results[i] = (byte)((Original - 10) + (byte) 'A');
 			}
 		}
 		return Results;
