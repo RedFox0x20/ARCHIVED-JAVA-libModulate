d6cb2338f0e266fe17afe7662023342004b670c4
Red (RedFox0x20) <redfox0x20@gmail.com>
2022-01-29 19:38:47 +0000

    Encoding
    Added mappings for Baudot encoding.

---
+++ libModulate/src/libModulate/Encoding.java
@@ -0,0 +1,12 @@
+package libModulate;
+
+public class Encoding {
+
+	public static final byte[] BAUDOT_ENCODING_LETTERS = new byte[] { 0, 'T', '\r', 'O', ' ', 'H', 'N', 'M', '\n', 'L',
+			'R', 'G', 'I', 'P', 'C', 'V', 'E', 'Z', 'D', 'B', 'S', 'Y', 'F', 'X', 'A', 'W', 'J', 1, 'U', 'Q', 'K', 2 };
+
+	public static final byte[] BAUDOT_ENCODING_FIGURES = new byte[] { 0, '5', '\r', '9', ' ', 'H', ',', '.', '\n', ')',
+			'4', '&', '8', '0', ':', ';', '3', '"', '$', '?', '\b', '6', '!', '/', '-', '2', '\'', 1, '7', '(', 2 };
+	
+	
+}
